376e5b8e9e3aa8a89bfa832f3adc23ae7cd54d95
Yousif Touma <youto814@student.liu.se>
2015-03-04 09:54:52 +0100

    add wall class

---
+++ core/src/com/mygdx/game/Wall.java
@@ -0,0 +1,22 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Wall, floor and ceiling class used for platformfunctionality. Detects collision.
+ */
+public class Wall extends Entity {
+    private Vector2 size;
+
+    /**
+     * @param sprite Sprite to represent wall
+     * @param position Vector2 (x,y) bottomleft position of sprite, 0,0 bottomleft in frame
+     * @param size Vector2 (width, height)
+     */
+    public Wall(final Sprite sprite, final Vector2 position, final Vector2 size) {
+	super(sprite, position);
+	this.size = size;
+	this.setSize(size.x, size.y);
+    }
+}
