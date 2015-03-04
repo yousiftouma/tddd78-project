6d799e24ccae995357a0cc5e3ecc756ab540749a
Yousif Touma <youto814@student.liu.se>
2015-03-04 11:51:20 +0100

    fixes to the desktop launcher

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,11 +4,22 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
-public class DesktopLauncher {
+/**
+ * launches game on desktop
+ */
+public final class DesktopLauncher {
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1080;
 		config.height = 720;
+	    	// result ignored since frame is created upon creation
 		new LwjglApplication(new MyGdxGame(), config);
 	}
+
+    /**
+     * not supposed to be called
+     */
+    private DesktopLauncher() {
+    }
 }
