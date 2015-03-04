f694d88511c735b81b8c782c759ef0138b668b97
Yousif Touma <youto814@student.liu.se>
2015-03-04 09:39:31 +0100

    remove constructor with size parameter

--- core/src/com/mygdx/game/Entity.java
+++ core/src/com/mygdx/game/Entity.java
@@ -6,31 +6,35 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Entity extends Sprite{
     private Sprite sprite;
-    private Vector2 pos;
+    private Vector2 position;
     private Vector2 size;
 
-    public Entity(Sprite sprite, Vector2 pos) {
+    public Entity(Sprite sprite, Vector2 position) {
         this.sprite = sprite;
-        this.pos = pos;
+        this.position = position;
         this.set(sprite);
-        this.setPosition(pos.x, pos.y);
+        this.setPosition(position.x, position.y);
     }
 
-    public Entity(Sprite sprite, Vector2 pos, Vector2 size) {
+    /**
+     * Move to wallentity where it has its only use
+     * @return
+     */
+    /*public Entity(Sprite sprite, Vector2 position, Vector2 size) {
         this.sprite = sprite;
-        this.pos = pos;
+        this.position = position;
         this.size = size;
         this.set(sprite);
         this.setSize(size.x, size.y);
-        this.setPosition(pos.x, pos.y);
-    }
+        this.setPosition(position.x, position.y);
+    }*/
 
     public Sprite getSprite() {
         return sprite;
     }
 
-    public Vector2 getPos() {
-        return pos;
+    public Vector2 getPosition() {
+        return position;
     }
 
     @Override
